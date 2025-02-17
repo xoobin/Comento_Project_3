81ea790e360c48022e5b2174d2be2ff9646b286f
xoobin <122351180+xoobin@users.noreply.github.com>
2025-02-18 02:22:10 +0900

    Add files via upload

---
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/dto/YearCountDto.java
@@ -0,0 +1,11 @@
+package com.demo.comentoStatistic.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class YearCountDto {
+    private String year;
+    private Integer totCnt;
+}
---
+++ comentoStatistic/src/main/java/com/demo/comentoStatistic/dto/YearMonthCountDto.java
@@ -0,0 +1,11 @@
+package com.demo.comentoStatistic.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class YearMonthCountDto {
+    String yearMonth;
+    Integer totCnt;
+}
